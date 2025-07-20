edf5e1f32b4303e772880e00e7f0fa9668b52bfc
Johnvee Mandal <johnvee.mandal@GXCHLAP00694.local>
2025-07-20 22:12:57 +0800

    Add Lab 8: GET all products endpoint in controller with in-memory repo

---
+++ springboot-crashcourse/session2/lab7/springboot-lab7/src/main/java/com/example/springboot_lab7/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.example.springboot_lab7.controller;
+
+import com.example.springboot_lab7.product.Product;
+import com.example.springboot_lab7.product.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
